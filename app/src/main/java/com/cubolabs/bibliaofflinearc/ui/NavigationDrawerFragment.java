81c68b2bc4a29dd12672d6e31432b4247e5d0cff
Diego Ponciano <dgo.ponciano@gmail.com>
2014-07-11 13:13:40 -0300

    disabled actionBar drawer

--- app/src/main/java/com/cubolabs/bibliaofflinearc/ui/NavigationDrawerFragment.java
+++ app/src/main/java/com/cubolabs/bibliaofflinearc/ui/NavigationDrawerFragment.java
@@ -85,7 +85,8 @@ public class NavigationDrawerFragment extends Fragment {
     public void onActivityCreated (Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         // Indicate that this fragment would like to influence the set of actions in the action bar.
-        setHasOptionsMenu(true);
+        // TODO: re-enable actionBar drawer
+        setHasOptionsMenu(false);
     }
 
     @Override
