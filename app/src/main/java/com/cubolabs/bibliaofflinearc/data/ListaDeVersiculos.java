00429867e17332e891f9b148b643c0cc025bff78
Diego Ponciano <me@diegoponciano.com>
2014-07-16 00:49:53 -0300

    lista versiculos returns text without verse number

--- app/src/main/java/com/cubolabs/bibliaofflinearc/data/ListaDeVersiculos.java
+++ app/src/main/java/com/cubolabs/bibliaofflinearc/data/ListaDeVersiculos.java
@@ -1,6 +1,7 @@
 package com.cubolabs.bibliaofflinearc.data;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import android.app.Activity;
@@ -29,26 +30,29 @@ public class ListaDeVersiculos {
 		livroDao = daoSession.getLivroDao();
 		palavraDao = daoSession.getPalavraDao();
 	}
-	
+
+    public ArrayList<Palavra> PorLivroECapitulo(String nomeDoLivro, int capitulo) {
+        Livro livro = livroDao.queryBuilder()
+                .where(LivroDao.Properties.Nome.eq(nomeDoLivro)).unique();
+
+        QueryBuilder<Palavra> pqb = palavraDao.queryBuilder();
+        List<Palavra> versiculos = pqb.where(pqb.and(
+                        PalavraDao.Properties.Id_livro.eq(livro.getId()),
+                        PalavraDao.Properties.Capitulo.eq(capitulo))
+        )
+                .orderAsc(PalavraDao.Properties.Versiculo)
+                .list();
+
+        return new ArrayList<Palavra>(versiculos);
+    }
+
 	public ArrayList<String> PorCapitulo(String nomeDoLivro, int capitulo) {
-		Livro livro = livroDao.queryBuilder()
-				.where(LivroDao.Properties.Nome.eq(nomeDoLivro)).unique();
-		
-		QueryBuilder<Palavra> pqb = palavraDao.queryBuilder();
-		List<Palavra> versiculos = pqb.where(pqb.and(
-											PalavraDao.Properties.Id_livro.eq(livro.getId()), 
-											PalavraDao.Properties.Capitulo.eq(capitulo))
-											)
-											.orderAsc(PalavraDao.Properties.Versiculo)
-											.list();
+        ArrayList<Palavra> versiculos = this.PorLivroECapitulo(nomeDoLivro, capitulo);
 		
 		final ArrayList<String> listaDeNomes = new ArrayList<String>();
 		
         for (int i = 0; i < versiculos.size(); ++i) {
-        	String numeroVersiculo = Short.toString(versiculos.get(i).getVersiculo());
-        	String versiculoInteiro = numeroVersiculo + "  " + versiculos.get(i).getTexto();
-        	
-        	listaDeNomes.add(versiculoInteiro);
+            listaDeNomes.add(versiculos.get(i).getTexto());
         }
         
         return listaDeNomes;
